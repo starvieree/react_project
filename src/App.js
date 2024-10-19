44a1fcc5e134fbfa3b85929d94c1fb4192c1b12b
starvieree <ainurreza90788@gmail.com>
2024-10-18 17:57:29 -0700

    setting font dan desain layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import NavbarComponent from "./components/NavbarComponent";
+import { Col, Row } from 'react-bootstrap';
+import { Hasil, NavbarComponent, ListCategories } from './components/index';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <div className='mt-2'>
+        <Row>
+          <ListCategories />
+          <Col>
+            <h4>Daftar Product</h4>
+            <hr />
+          </Col>
+          <Hasil />
+        </Row>
+      </div>
     </div>
   );
 }
--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,28 +1,22 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from 'react';
+import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
 
-function BasicExample() {
+const NavbarComponent = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home"><strong>Kasir</strong> App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
@@ -31,4 +25,4 @@ function BasicExample() {
   );
 }
 
-export default BasicExample;
+export default NavbarComponent;
