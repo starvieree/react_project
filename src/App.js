9129139fa24a956b61585ffb8137ce11236cad0e
starvieree <ainurreza90788@gmail.com>
2024-10-19 08:03:31 -0700

    menampilkan menu product

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import React from 'react';
-import { Col, Row } from 'react-bootstrap';
-import { Hasil, NavbarComponent, ListCategories } from './components/index';
+import React, { Component } from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+import { Hasil, NavbarComponent, Menus, ListCategories } from './components/index';
+import { API_URL } from "./utils/constants";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent />
-      <div className='mt-2'>
-        <Row>
-          <ListCategories />
-          <Col>
-            <h4>Daftar Product</h4>
-            <hr />
-          </Col>
-          <Hasil />
-        </Row>
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       menus : [],
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL+"products")
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+  
+  render() {
+    const { menus } = this.state
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <div className='mt-2'>
+          <Container fluid>
+            <Row>
+              <ListCategories />
+              <Col>
+                <h4>
+                  <strong>Daftar Product</strong>
+                </h4>
+                <hr />
+                <Row>
+                  {menus && menus.map((menu) => (
+                    <Menus 
+                      key = {menu.id}
+                      menu = {menu}
+                    />
+                  ))}
+                </Row>
+              </Col>
+              <Hasil />
+            </Row>
+          </Container>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
-
-export default App;
