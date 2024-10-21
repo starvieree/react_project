02690234225c40b0f8675deb8ccd63c906d179b6
starvieree <ainurreza90788@gmail.com>
2024-10-21 15:07:52 -0700

    membuat keranjang pesanan

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { Hasil, NavbarComponent, Menus, ListCategories } from './components/index';
 import { API_URL } from "./utils/constants";
 import axios from "axios";
+import swal from 'sweetalert';
 
 export default class App extends Component {
   constructor(props) {
@@ -11,6 +12,7 @@ export default class App extends Component {
     this.state = {
        menus : [],
        categoriYangDipilih: "Makanan",
+       keranjangs : []
     }
   }
 
@@ -23,7 +25,31 @@ export default class App extends Component {
       })
       .catch(error => {
         console.log(error);
+      });
+
+    axios
+      .get(API_URL+"keranjangs")
+      .then(res => {
+        const keranjangs = res.data;
+        this.setState({ keranjangs });
       })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.keranjangs !== prevState.keranjangs) {
+      axios
+      .get(API_URL+"keranjangs")
+      .then(res => {
+        const keranjangs = res.data;
+        this.setState({ keranjangs });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
   }
 
   changeCategory = (value) => {
@@ -43,9 +69,63 @@ export default class App extends Component {
       })
 
   }
+
+  masukKeranjang = (value) => {
+
+    axios
+      .get(API_URL+"keranjangs?product.id=" + value.id)
+      .then(res => {
+        if (res.data.length === 0) {
+          const keranjang = {
+            jumlah: 1,
+            total_harga: value.harga,
+            product: value
+          }
+      
+          axios
+            .post(API_URL+"keranjangs", keranjang)
+            .then(res => {
+              swal({
+                title: "Sukses Masuk Keranjang",
+                text: "Sukses Masuk Keranjang " + keranjang.product.nama,
+                icon: "success",
+                button: false,
+                timer: 1000
+              });
+            })
+            .catch(error => {
+              console.log(error);
+            })
+        } else {
+          const keranjang = {
+            jumlah: res.data[0].jumlah + 1,
+            total_harga: res.data[0].total_harga + value.harga,
+            product: value
+          }
+
+          axios
+            .put(API_URL+"keranjangs/"+res.data[0].id, keranjang)
+            .then(res => {
+              swal({
+                title: "Sukses Masuk Keranjang",
+                text: "Sukses Masuk Keranjang " + keranjang.product.nama,
+                icon: "success",
+                button: false,
+                timer: 1000
+              });
+            })
+            .catch(error => {
+              console.log(error);
+            })
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
   
   render() {
-    const { menus, categoriYangDipilih } = this.state
+    const { menus, categoriYangDipilih, keranjangs } = this.state
     return (
       <div className="App">
         <NavbarComponent />
@@ -63,11 +143,12 @@ export default class App extends Component {
                     <Menus 
                       key = {menu.id}
                       menu = {menu}
+                      masukKeranjang = {this.masukKeranjang}
                     />
                   ))}
                 </Row>
               </Col>
-              <Hasil />
+              <Hasil keranjangs={keranjangs} />
             </Row>
           </Container>
         </div>
--- src/components/Menus.js
+++ src/components/Menus.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Card, Col } from "react-bootstrap";
 import { numberWithCommas } from "../utils/utils";
 
-const Menus = ({ menu }) => {
+const Menus = ({ menu, masukKeranjang }) => {
     return (
         <Col md={4} xs={6} className='mb-4'>
-            <Card className='shadow'>
+            <Card className='shadow' onClick = {() => masukKeranjang(menu)}>
                 <Card.Img variant="top" src={"assets/images/" + menu.category.nama.toLowerCase() + "/" + menu.gambar} />
                 <Card.Body>
                     <Card.Title>{menu.nama}</Card.Title>
